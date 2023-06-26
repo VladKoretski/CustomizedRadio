420b2490dcda39ac6880b9c52a51780f31334242
Vladko1973 <vpk1973@yandex.ru>
2023-06-26 12:56:49 +0300

    second commit

--- src/main/java/ru/netology/CustomizedRadio.java
+++ src/main/java/ru/netology/CustomizedRadio.java
@@ -1,2 +1,104 @@
-package ru.netology;public class CustomizedRadio {
+package ru.netology;
+
+public class CustomizedRadio {
+
+    // Данные номера радиостанции
+    private final int minRadioStationNumber = 0;
+    // Данные громкости звука
+    private final int minVolumeOfTheRadio = 0;
+    private final int maxVolumeOfTheRadio = 100;
+    private int maxRadioStationNumber = 9;
+    private int currentRadioStation = minRadioStationNumber;
+    private int currentVolumeOfTheRadio = minVolumeOfTheRadio;
+
+    // конструктор радио по умолчанию
+    public CustomizedRadio() {
+    }
+
+    // конструктор customized radio
+    public CustomizedRadio(int RangeOfStations) {
+
+        this.maxRadioStationNumber = minRadioStationNumber + RangeOfStations - 1;
+    }
+
+    //геттеры класса радио - номер радиостанции
+    public int getMinRadioStationNumber() {
+
+        return minRadioStationNumber;
+    }
+
+    public int getMaxRadioStationNumber() {
+
+        return maxRadioStationNumber;
+    }
+
+    public int getCurrentRadioStation() {
+
+        return currentRadioStation;
+    }
+
+    //геттеры класса радио - уровень звука
+    public int getMinVolumeOfTheRadio() {
+
+        return minRadioStationNumber;
+    }
+
+    public int getMaxVolumeOfTheRadio() {
+
+        return maxVolumeOfTheRadio;
+    }
+
+    public int getCurrentVolumeOfTheRadio() {
+
+        return currentVolumeOfTheRadio;
+    }
+
+    // Методы выбора номера станции
+    public void setNewRadioStation(int radioStation) {
+        if (radioStation < minRadioStationNumber) {
+            return;
+        }
+        if (radioStation > maxRadioStationNumber) {
+            return;
+        }
+        currentRadioStation = radioStation;
+    }
+
+    public void next() {
+        if (currentRadioStation == maxRadioStationNumber) {
+            currentRadioStation = minRadioStationNumber;
+        } else currentRadioStation = currentRadioStation + 1;
+    }
+
+    public void prev() {
+        if (currentRadioStation == minRadioStationNumber) {
+            currentRadioStation = maxRadioStationNumber;
+        } else currentRadioStation = currentRadioStation - 1;
+    }
+
+    // Методы установки громкости
+
+    public void setNewVolume(int newVolume) {
+        if (newVolume < minVolumeOfTheRadio) {
+            return;
+        }
+        if (newVolume > maxVolumeOfTheRadio) {
+            return;
+        }
+        currentVolumeOfTheRadio = newVolume;
+    }
+
+    public void setVolumeUpOnePointUp() {
+        if (currentVolumeOfTheRadio == maxVolumeOfTheRadio) {
+            return;
+        }
+        currentVolumeOfTheRadio = currentVolumeOfTheRadio + 1;
+    }
+
+    public void setVolumeOnePointDown() {
+        if (currentVolumeOfTheRadio == minVolumeOfTheRadio) {
+            return;
+        }
+        currentVolumeOfTheRadio = currentVolumeOfTheRadio - 1;
+    }
 }
--- src/test/java/CustomizedRadioTest.java
+++ src/test/java/CustomizedRadioTest.java
@@ -1,2 +1,198 @@
-package PACKAGE_NAME;public class CustomizedRadioTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.CustomizedRadio;
+
+public class CustomizedRadioTest {
+// Тестирование конструктов
+
+    @Test
+    public void shouldCheckOneParameterConstructor() {
+        CustomizedRadio radio = new CustomizedRadio (15);
+
+        Assertions.assertEquals(0, radio.getMinRadioStationNumber());
+        Assertions.assertEquals(14, radio.getMaxRadioStationNumber());
+        Assertions.assertEquals(0, radio.getCurrentRadioStation());
+        Assertions.assertEquals(0, radio.getMinVolumeOfTheRadio());
+        Assertions.assertEquals(100, radio.getMaxVolumeOfTheRadio());
+        Assertions.assertEquals(0, radio.getCurrentVolumeOfTheRadio());
+    }
+
+    @Test
+    public void shouldCheckOutTheDefaultDataConstructor() {
+        CustomizedRadio radio = new CustomizedRadio();
+        Assertions.assertEquals(0, radio.getMinRadioStationNumber());
+        Assertions.assertEquals(9, radio.getMaxRadioStationNumber());
+        Assertions.assertEquals(0, radio.getCurrentRadioStation());
+        Assertions.assertEquals(0, radio.getMinVolumeOfTheRadio());
+        Assertions.assertEquals(100, radio.getMaxVolumeOfTheRadio());
+        Assertions.assertEquals(0, radio.getCurrentVolumeOfTheRadio());
+    }
+
+
+    //Тестирование методов выбора станции
+    //Тестирование метода установки новой станции:
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "1, 1",
+            "9, 9",
+            "8, 8",
+            "10, 0", //по умолчанию первоначальная станция 0
+            "5, 5",
+            "-1, 0" //по умолчанию первоначальная станция 0
+    })
+
+    public void shouldSetRadioStation(int newRadioStation, int expected) {
+        CustomizedRadio radio = new CustomizedRadio (10);
+        radio.setNewRadioStation(newRadioStation);
+        int actual = radio.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Тестирование метода пролистования станций Next с граничными условиями:
+    @ParameterizedTest
+    @CsvSource({
+            "4, 5",
+            "8, 9",
+            "9, 0",
+            "0, 1",
+            "1, 2"
+    })
+
+    public void shouldSetNextRadioStation(int currentStation, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        radio.setNewRadioStation(currentStation);
+        radio.next();
+        int actual = radio.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    //Тестирование метода пролистования станций Prev с граничными условиями:
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3",
+            "9, 8",
+            "0, 9",
+            "1, 0",
+            "8, 7"
+    })
+
+    public void shouldSetPrevRadioStation(int currentStation, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        radio.setNewRadioStation(currentStation);
+        radio.prev();
+        int actual = radio.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 5",
+            "13, 14",
+            "14, 0",
+            "0, 1",
+            "1, 2"
+    })
+
+    public void shouldSetNextRadioStationIf15RadioStations(int currentStation, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(15);
+        radio.setNewRadioStation(currentStation);
+        radio.next();
+        int actual = radio.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "4, 3",
+            "14, 13",
+            "0, 14",
+            "1, 0",
+    })
+
+    public void shouldSetPrevRadioStationif15RadioStations(int currentStation, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(15);
+        radio.setNewRadioStation(currentStation);
+        radio.prev();
+        int actual = radio.getCurrentRadioStation();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+
+    //Тестирование методов громкости
+    //Тестирования метода установки громкости - служебного метода для дальнейшего тестирования
+    //Тест с учетом классов эквивалентности и граничными условиями
+    @ParameterizedTest
+    @CsvSource({
+            "5, 5",
+            "0, 0",
+            "100, 100",
+            "99, 99",
+            "101, 0",
+            "-1, 0",
+            "1, 1"
+    })
+
+    public void shouldSetNewVolume(int newVolume, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        radio.setNewVolume(newVolume);
+        int actual = radio.getCurrentVolumeOfTheRadio();
+
+        Assertions.assertEquals(actual, expected);
+    }
+
+    //Тестирование getCurrentVolume на значение по умолчанию
+    @Test
+    public void shouldGetCurrentVolume() {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        int expected = 0;
+        int actual = radio.getCurrentVolumeOfTheRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Тестирование метода на повышение громкости с учетом граничных условий
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1",
+            "1, 2",
+            "11, 12",
+            "99, 100",
+            "100, 100"
+    })
+
+    public void shouldSetVolumeOnePointUp(int currentVolume, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        radio.setNewVolume(currentVolume);
+        radio.setVolumeUpOnePointUp();
+        int actual = radio.getCurrentVolumeOfTheRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Тестирование метода на снижение громкости с учетом граничных условий
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "12, 11",
+            "7, 6",
+            "99, 98",
+            "100, 99"
+    })
+
+    public void shouldSetVolumeOnePointDown(int currentVolume, int expected) {
+        CustomizedRadio radio = new CustomizedRadio(10);
+        radio.setNewVolume(currentVolume);
+        radio.setVolumeOnePointDown();
+        int actual = radio.getCurrentVolumeOfTheRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
